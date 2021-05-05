948ed21e8534624cbacbadcfb342a00adadd0a8a
Nasim Hosseini <79575160+nasim-h@users.noreply.github.com>
2021-05-05 20:33:45 +0430

    Add files via upload

---
+++ 2.java
@@ -0,0 +1,25 @@
+package team14
+
+ import javax.swing.JOptionPane;
+
+public class Workshop4_2 {
+    public static void main(String[] args) {
+        int arraySize;
+        String size = JOptionPane.showInputDialog(null,
+                "Please enter number of names: ",
+                "Specifying List Size",
+                JOptionPane.QUESTION_MESSAGE) ;
+        arraySize = Integer.parseInt(size);
+        String[] myList = new String[arraySize];
+        
+        for(int i = 0; i < myList.length; i++){
+            myList[i] = JOptionPane.showInputDialog(null,
+                    String.format("Please enter the name #%d:",i),
+                    "#" + i);
+        }
+        for(int i = 0; i < myList.length; i++){
+            System.out.println(myList[i]);
+        }
+    }
+    
+}
